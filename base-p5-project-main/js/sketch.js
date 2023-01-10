0403455eccfff55002d3d9a704eb2321d5b6f6e4
louisealexander <115469352+louisealexander@users.noreply.github.com>
2023-01-10 11:58:14 +0000

    sine and cosine green and red waves

--- base-p5-project-main/js/sketch.js
+++ base-p5-project-main/js/sketch.js
@@ -1,17 +1,36 @@
+let x = 0; 
+let y = 0;
+let theta = 0;
+let inc = 0.05;
+let offset = 0; //the amount offset from beginning of wave
+let amplitude;
+let frequency;
+let maxAngle;
 function setup() {
+    amplitude = windowHeight /8;
+    frequency = windowWidth /8;
+    maxAngle = (windowWidth/frequency) * TWO_PI;
     createCanvas(windowWidth, windowHeight);
-    let x = 0; 
-    let y = 0;
-    let theta = 0;
-    let amplitude = windowHeight*0.25;
-    let frequency = windowWidth*0.20;
-    let maxAngle = (windowWidth/frequency) * TWO_PI;
-    while(theta < maxAngle){
-        y = sin(theta) * amplitude;
-        ellipse(x, y + height*0.5, 16);
-        theta += 0.2;
-        x = (theta/maxAngle) * windowWidth;
-    }
 }
 function draw() {
+    background(0);
+    noStroke();
+    while(theta < maxAngle + offset){
+        // sine wave 2
+        y = sin(theta - 0.2) * amplitude;
+        fill(100, 0, 0);
+        ellipse(x, y + height*0.5, 16);
+        // sine wave 1
+        y = sin(theta) * amplitude;
+        fill(255, 0, 0);
+        ellipse(x, y + height*0.5, 16);
+        // cosine wave
+        y = cos(theta) * amplitude;
+        fill(212, 255, 0);
+        ellipse(x, y + height*0.5, 16);
+        theta += 0.2;
+        x = ((theta - offset)/maxAngle) * windowWidth;
+    }
+    offset += inc;
+    theta = offset;
 }
